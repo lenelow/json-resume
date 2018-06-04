30f4fceb24573d21b88f5b0032ee6d5d670baf65
Leah Enelow <lenelow@students.terpmail.edu>
2018-06-04 11:11:50 -0400

    resume repository

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+const fs = require('fs')
+
+fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
+    if (err) {
+        console.error(err)
+    } 
+    else {
+        console.log(data)
+        return data    
+    }
+})
